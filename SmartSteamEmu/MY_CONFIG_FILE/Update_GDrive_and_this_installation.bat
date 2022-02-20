29c7d36ca1a4188666d71b8bb94096f8737b8143
David <45155292+DavoDC@users.noreply.github.com>
2022-02-21 00:25:48 +0800

    Added copier script

---
+++ SmartSteamEmu/MY_CONFIG_FILE/Update_GDrive_and_this_installation.bat
@@ -0,0 +1,19 @@
+@echo off
+echo.
+
+
+echo "Copying local SmartSteamEmu.ini to GDrive (linked to guide)..."
+cp "SmartSteamEmu.ini" "C:\Users\David\Google Drive\Computery\Documents\GoogleDocs\Davo's_L4D2_Guide\SmartSteamEmu.ini"
+echo "Done"
+echo.
+
+
+echo "Copying local SmartSteamEmu.ini to this installation's install directory..."
+echo "Done"
+cp "SmartSteamEmu.ini" "..\..\SmartSteamEmu.ini"
+echo.
+
+
+echo "Finished!"
+echo.
+@pause
