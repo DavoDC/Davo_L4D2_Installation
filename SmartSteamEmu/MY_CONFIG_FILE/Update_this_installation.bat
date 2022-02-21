506ab38d1894ced3751e4c0f85b7c6474a15eb37
David <45155292+DavoDC@users.noreply.github.com>
2022-02-21 10:18:20 +0800

    Disabled GDrive copier, Renamed extra

--- SmartSteamEmu/MY_CONFIG_FILE/Update_this_installation.bat
+++ SmartSteamEmu/MY_CONFIG_FILE/Update_this_installation.bat
@@ -2,10 +2,6 @@
 echo.
 
 
-echo "Copying local SmartSteamEmu.ini to GDrive (linked to guide)..."
-cp "SmartSteamEmu.ini" "C:\Users\David\Google Drive\Computery\Documents\GoogleDocs\Davo's_L4D2_Guide\SmartSteamEmu.ini"
-echo "Done"
-echo.
 
 
 echo "Copying local SmartSteamEmu.ini to this installation's install directory..."
